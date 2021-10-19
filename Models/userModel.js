6d8b69e225dc00969f6fc4f437546994148079c3
Kelvin Popovic <kelvin.popovic@gmail.com>
2021-10-19 23:15:33 +0200

    Bug fix o_o

--- Models/userModel.js
+++ Models/userModel.js
@@ -1,12 +1,11 @@
 const e = require('express');
 const mongoose = require("mongoose");
 
-var userSchema = new mongoose.Schema({
-    _id : new mongoose.Types.ObjectId(),
+const userSchema = new mongoose.Schema({
+    _id : mongoose.Schema.Types.ObjectId(),
     username : String,
     password : String,
     email : String
 });
 
-var userModel = mongoose.model('users', userSchema);
-module.exports = mongoose.model("Users", userModel);
+module.exports = mongoose.model("Users", userSchema);
