15d586607a2759e2519c26d2772486fe23945d53
AJ The Developer <ajthedeveloper@gmail.com>
2021-11-17 13:56:48 +0200

    Added list get and update requests

--- api/controllers/sprint.js
+++ api/controllers/sprint.js
@@ -139,4 +139,59 @@ exports.sprint_create = (req, res, next) =>{
         });
     })
 }
-        
+
+exports.sprint_get_lists = (req, res, next) => {
+    const sprID = req.params._id;
+    Sprints.find({_id: req.params._id})
+    .select('sprName lists')
+    .exec()
+    .then(doc => {
+        if(doc)
+        {
+            res.status(200).json({
+                sprName : doc.sprName,
+                lists: doc.lists,
+                request: {
+                    type : 'GET',
+                    description : 'Get all lists and tasks for sprint',
+                    url : 'https://mysterious-reef-01698.herokuapp.com/' + id
+                }
+            });
+        }
+        else
+        {
+            res.status(404).json({
+                message: 'Entry not found in database'
+            });
+        }
+    })
+}
+
+exports.sprint_update_lists = (req, res, next) =>{
+    const id = req.params._id;
+    const updateOps = {};
+
+    for(const ops of req.body)
+    {
+        updateOps[ops.propName] = ops.value;
+    }
+
+    Sprints.update({_id: id}, {$set: updateOps}).exec()
+    .then(result => {
+        res.status(200).json({
+            message: 'Lists updated',
+            request : {
+                type : 'GET',
+                url : 'https://mysterious-reef-01698.herokuapp.com/' + id
+            }
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
+
+}
+    
