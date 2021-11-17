dff8f0f6332cb1369bd806721fdaf40d62764436
Neil Foxcroft <neilfoxcroftpro@gmail.com>
2021-11-17 13:13:18 +0200

    Updates Sprint Schema
    Added tasks to the Sprint schema as Objects to reference
    
    Co-Authored-By: AJ The Developer <ajthedeveloper@gmail.com>

--- api/models/sprintSchema.js
+++ api/models/sprintSchema.js
@@ -4,7 +4,20 @@ const mongoose = require('mongoose');
 const sprintSchema =  mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     sprName: {type: String, required: false},
-    project: {type: String, ref: 'Projects'}
+    project: {type: String, ref: 'Projects'},
+    lists: [
+        {
+            _id: mongoose.Schema.Types.ObjectId,
+            listName: {type: String, required: true},
+            tasks: [
+                {
+                    _id: mongoose.Schema.Types.ObjectId,
+                    taskName: {type: String, required: true},
+                    taskUsers: [{type: String, ref: 'User'}],
+                } 
+            ]
+        }
+    ]
 });
 
 module.exports = mongoose.model('Sprint',sprintSchema);
